2b8049ae7d2b7b6ca46178ac531d405d7f6ba5e1
Guilherme Valença Saunders <guilhermevalenca.s7@gmail.com>
2025-11-19 15:37:16 -0300

    feature(historyCard): added bg-white and adjusted the size of internal elements and the height of the element

--- client/src/components/ui/details/HistoryCard.tsx
+++ client/src/components/ui/details/HistoryCard.tsx
@@ -14,22 +14,24 @@ interface HistoryCardProps {
 // Componente HistoryCard without onClick implementation on the arrowback image and without the conection with the backend data
 const HistoryCard: React.FC<HistoryCardProps> = ({ date, time, type, doctorName, onClick }) => {
   return (
-    <div className="flex items-center justify-between w-full p-6 bg-gray-100 rounded-xl shadow-md">
+    <div className="flex items-center justify-between w-full h-16 p-6 bg-gray-100 rounded-xl shadow-md ">
 
-      <div className="flex flex-col items-center justify-center border-r pr-6 border-gray-300">
-        <p className="font-bold text-sm">{date}</p>
-        <p className="font-bold text-sm">{time}</p>
+      <div className="flex flex-col items-center justify-center border-r p-2 bg-white rounded-xl h-auto">
+        <p className="font-bold text-xs">{date}</p>
+        <p className="font-bold text-xs">{time}</p>
       </div>
 
       <div className="flex-grow flex justify-start mx-4 min-w-0 p-6">
-        <p className="font-bold text-base whitespace-nowrap overflow-hidden text-ellipsis">{type}</p>
+        <p className="font-bold text-sm whitespace-nowrap overflow-hidden text-ellipsis">{type}</p>
       </div>
 
       <div className="flex items-center space-x-4 pr-10 pl-6">
-        <p className="text-sem whitespace-nowrap">{doctorName}</p>
+        <p className="text-sem whitespace-nowrap text-sm">{doctorName}</p>
       </div>
 
-      <Image src={ArrowBack} alt="Voltar" width={20} height={10} onClick={onClick}/> {/*i need to implement onclick on this component*/}
+      <div className="flex items-center pl-4 flex-shrink-0">
+        <Image src={ArrowBack} alt="Voltar" width={14} height={8} /> {/*need to implement onclick on this component*/}
+      </div>
     </div>
   );
 }
