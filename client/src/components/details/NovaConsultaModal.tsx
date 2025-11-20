4328b2920ffbf3e6fb4e24f616057cdffa68950d
Alisson <asb3@cin.ufpe.br>
2025-11-20 17:41:59 -0300

    modal finalizado

--- client/src/components/details/NovaConsultaModal.tsx
+++ client/src/components/details/NovaConsultaModal.tsx
@@ -5,9 +5,7 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import Image from "next/image";
@@ -30,7 +28,9 @@ export function NovaConsultaModal() {
   const [horarioAtendimento, setHorarioAtendimento] = useState('');
   const [tipoConsulta, setTipoConsulta] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
+
+  // Essa função será chamada ao clicar em finalizar. apertando f12, verá as informações enviadas
+  const Submit = (e: React.FormEvent) => {
     e.preventDefault();
 
     console.log({
@@ -53,29 +53,25 @@ export function NovaConsultaModal() {
       </DialogTrigger>
 
       <DialogContent
-            className="
-              w-full
-              max-w-[824px]
-              max-h-[90vh]
-              overflow-y-auto
-
-              rounded-[24px]
-              bg-white
-
-              p-6
-              sm:p-[48px]
-
-              space-y-6
-              sm:space-y-[29px]
-            "
-          >
+        className="
+            w-full
+            max-w-[824px]
+            h-auto      
+            max-h-[90vh]  
+            rounded-[24px]     
+            bg-white 
+            p-[48px]          
+            flex                 
+            flex-col            
+            gap-[29px]      
+          "
+        >
 
 
-        {/* Logo */}
+        {/* Logo. DialogHeader serve para acessibilidade */}
         <div className="flex justify-center mb-2">
           <Image src={LogoCITi} width={189} height={74} alt="Citi Logo" />
         </div>
-
         <DialogHeader className="flex justify-center">
         <p className="text-center text-lg font-normal leading-tight">
           <span className="font-semibold">
@@ -87,16 +83,27 @@ export function NovaConsultaModal() {
       </DialogHeader>
 
 
-        <form onSubmit={handleSubmit} className="mt-4">
+        <form onSubmit={Submit} className="mt-4">
 
-          {/* GRID DE 2 COLUNAS */}
+          {/*Em celular, cria-se 1 coluna, em notebook, cria-se duas
+          selectContent: tipo uma caixa, selectGroup: uma pasta dentro da caixa e selectLabel: o tipo dos documentos (texto aqui)*/}
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
 
-            {/* Tipo de consulta */}
+            {/* Tipo de consulta. Conecta o Select ao estado tipoConsulta.  */}
             <div className="flex flex-col space-y-1">
-              <Label className="text-sm">Tipo de consulta</Label>
+              <Label>
+                Tipo de consulta
+              </Label>
               <Select value={tipoConsulta} onValueChange={setTipoConsulta}>
-                <SelectTrigger className="w-full">
+                <SelectTrigger className="
+                  w-full               
+                  h-[50px]             
+                  rounded-[8px]        
+                  border-[1px]         
+                  border-[#101010]     
+                  p-[16px]             
+                  text-base            
+                  placeholder:text-gray-400">
                   <SelectValue placeholder="Selecione aqui" />
                 </SelectTrigger>
                 <SelectContent>
@@ -112,9 +119,11 @@ export function NovaConsultaModal() {
               </Select>
             </div>
 
-            {/* Médico Responsável */}
+            {/* Médico Responsável. Usa o useState para pegar o que for digitado. */}
             <div className="flex flex-col space-y-1">
-              <Label className="text-sm">Médico Responsável</Label>
+              <Label>
+                Médico Responsável
+              </Label>
               <Input
                 placeholder="Digite aqui..."
                 value={medico}
@@ -123,47 +132,75 @@ export function NovaConsultaModal() {
               />
             </div>
 
-            {/* Data */}
+            {/* Data. Também usa o useState para pegar a informação e o [&::-webkit-calendar-picker-indicator]:hidden, serve para
+            esconder o símbolo padrão do navegador*/}
             <div className="flex flex-col space-y-1">
-              <Label className="text-sm">Data do atendimento</Label>
-              <Input
+              <Label>
+                    Data do atendimento
+              </Label>
+              <div className="relative">
+                <Input
                 type="date"
                 value={dataAtendimento}
                 onChange={(e) => setDataAtendimento(e.target.value)}
                 required
+                className="
+                 [&::-webkit-calendar-picker-indicator]:hidden"
               />
+              <span className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none">
+                  {/* Ícone manual */}
+                  <Image 
+                    src={CalendarIcon} 
+                    width={20} 
+                    height={20} 
+                    alt="Calendário" 
+                  />
+                </span>
             </div>
+              </div>
+              
 
-            {/* Horário */}
+            {/* Horário. Basicamente a mesma coisa da data*/}
             <div className="flex flex-col space-y-1">
-              <Label className="text-sm">Horário do atendimento</Label>
+              <Label>
+                    Horário do atendimento
+              </Label>
               <div className="relative">
                 <Input
+                  type="time"
                   value={horarioAtendimento}
                   onChange={(e) => setHorarioAtendimento(e.target.value)}
                   required
-                  className="pr-10" 
+                  className=" 
+                  [&::-webkit-calendar-picker-indicator]:hidden"
                 />
-                <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+                <span className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none">
+                  {/* Ícone manual */}
                   <Image 
                     src={AlarmIcon} 
                     width={20} 
                     height={20} 
                     alt="Relógio" 
                   />
-                </div>
+                </span>
               </div>
             </div>
           </div>
-
-          <Button
+            <Button
               type="submit"
               className="
-                w-full mt-12
-                bg-[#50E678] hover:bg-[#1FAA53]
-                text-white 
-                py-6 rounded-full 
-                text-md font-medium
+                w-full   
+                h-auto          
+                px-[32px]      
+                py-[12px]        
+                rounded-[24px]     
+                gap-[10px]       
+                bg-[#50E678]    
+                hover:bg-[#1FAA53]    
+                text-white           
+                flex items-center justify-center
+                text-md font-medium   
+                mt-6                 
               "
             >
               Finalizar cadastro
--- client/src/components/ui/label.tsx
+++ client/src/components/ui/label.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-base font-bold  text-black leading-[110%] "
 )
 
 const Label = React.forwardRef<
