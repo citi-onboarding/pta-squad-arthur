d38eb109e8deca590a9793efe90e3b83818b3cce
Arthur Torres <arthur.torresdl03@gmail.com>
2025-11-21 00:49:38 -0300

    refactor(server-structure): Implemented Zod and errorHandler

--- server/src/routes/UserRoutes.ts
+++ server/src/routes/UserRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import userController from 'src/controllers/UserController';
+import UserController from 'src/controllers/UserController';
 
 const UserRouter = Router();
 
-UserRouter.post("/", userController.create);
-UserRouter.get("/", userController.get);
-UserRouter.delete("/:id", userController.delete);
-UserRouter.patch("/:id", userController.update);
+UserRouter.post("/", UserController.create);
+UserRouter.get("/", UserController.findAll);
+UserRouter.get("/:id", UserController.findById);
+UserRouter.delete("/:id", UserController.delete);
+UserRouter.patch("/:id", UserController.update);
 
 export default UserRouter;
