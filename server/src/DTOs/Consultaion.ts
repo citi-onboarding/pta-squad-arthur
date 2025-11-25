7bcef672443e6fabdbd02a6fd772117e6776f92d
Guilherme Valença Saunders <guilhermevalenca.s7@gmail.com>
2025-11-25 19:59:45 -0300

    feat("crud-consultation"): enhance ConsultationSchema with detailed validation rules for fields

--- server/src/DTOs/Consultaion.ts
+++ server/src/DTOs/Consultaion.ts
@@ -1,11 +1,28 @@
 import { z } from 'zod';
 
 export const ConsultationSchema = z.object({
-  doctorName: z.string().min(1, "Nome é obrigatório"),
-  description: z.string().min(1, "A descrição da consulta é obrigatória"),
-  type: z.string().min(1, "O tipo de consulta é obrigatório"),
-  datetime: z.coerce.date(),
-  patientId: z.string().uuid(),
+
+  doctorName: z
+  .string({error: "O nome deve ser uma string"})
+  .nonempty({ message: "O nome é obrigatório, por favor informe-o."})
+  .regex(/^[a-zA-Z\s'áàãâéèêíìóòõôúùçÁÀÃÂÉÈÊÍÌÓÒÕÔÚÙÇ]+$/, {message: "O nome contém caracters inválidos"}),
+
+  description: z
+  .string({error: "A descrição deve ser uma string"})
+  .nonempty({ message: "A descrição da consulta é obrigatória."}),
+
+  type: z
+  .string({error: "O tipo da consulta deve ser uma string"})
+  .nonempty({message: "Por favor, informe o tipo de consulta."})
+  .regex(/^[a-zA-Z\s'áàãâéèêíìóòõôúùçÁÀÃÂÉÈÊÍÌÓÒÕÔÚÙÇ]+$/, {message: "O tipo deconsulta contém caracters inválidos, por favor revise-o"}),
+
+  datetime: z
+  .coerce.date(),
+
+
+  patientId: z
+  .string()
+  .uuid(),
 });
 
 // TODO: Exportar o UpdateConsultationSchema usando o .partial()
