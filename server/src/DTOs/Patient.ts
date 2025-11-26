2bffc91bd2c64e67b978072ee1546b2b46ca7d39
Alisson <asb3@cin.ufpe.br>
2025-11-26 09:34:17 -0300

    havia feito commit retirando inferencia de tipo, no cin, mas não foi, nao sei pq

--- server/src/DTOs/Patient.ts
+++ server/src/DTOs/Patient.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 // zod.object define que espera um objeto JSON (chave valor)
 // a mensagem será enviada ao front, caso haja erro
 export const PatientSchema = z.object({
-  name: z.string().min(1, "O nome do pet é obrigatório"), // Ajustado para Paciente
+  name: z.string().min(1, "O nome do pet é obrigatório"),
   tutorName: z.string().min(1, "O nome do tutor é obrigatório"),
   age: z.number().int().min(0, "A idade deve ser um número inteiro positivo"),
   species: z.string().min(1, "A espécie é obrigatória"),
--- server/src/repositories/PatientRepository.ts
+++ server/src/repositories/PatientRepository.ts
@@ -1,31 +1,31 @@
 import prisma from "@database";
-import { Prisma } from "@prisma/client";
+import { Prisma, Patient } from "@prisma/client";
 
 // essa parte serve para isolar o BD (muito legal :)) 
 class PatientRepository {
   
-  async create(data: Prisma.PatientUncheckedCreateInput) {
+  async create(data: Prisma.PatientUncheckedCreateInput): Promise<Patient> {
     return await prisma.patient.create({ data });
   }
 
-  async findAll() {
+  async findAll(): Promise<Patient[]> {
     return await prisma.patient.findMany();
   }
 
-  async findById(id: string) {
+  async findById(id: string): Promise<Patient| null> {
     return await prisma.patient.findUnique({
       where: { id },
     });
   }
 
-  async update(id: string, data: Prisma.PatientUpdateInput) {
+  async update(id: string, data: Prisma.PatientUpdateInput): Promise<Patient> {
     return await prisma.patient.update({
       where: { id },
       data,
     });
   }
 
-  async delete(id: string) {
+  async delete(id: string): Promise<Patient> {
     return await prisma.patient.delete({
       where: { id },
     });
