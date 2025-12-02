8f65e83ba717f3c472e90b8da1c2c0c9d4a454a3
Arthur Torres <arthur.torresdl03@gmail.com>
2025-12-02 00:48:05 -0300

    fix(main): Added cors for integration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import routes from "./routes";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import "@database";
 import { errorHandler } from "./middlewares/errorHandler"
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors()); 
 app.use(express.json());
 app.use(routes);
 app.use(express.static(__dirname + "/public"));
